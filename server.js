9a7075f7c53f20a9d8d7ba7a37ff9e61c15d06dd
firevillagetournament <firevillage.in@gmail.com>
2025-04-14 12:04:57 +0530

    server.js
    Signed-off-by: firevillagetournament <firevillage.in@gmail.com>

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 mongodb+srv://firevillage:<db_password>@firevillage.m7qlmft.mongodb.net/?retryWrites=true&w=majority&appName=firevillage
+mongoose.connect('mongodb://localhost:27017/firevillage', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
